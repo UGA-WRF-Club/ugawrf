507488e634019f138a98366634f326420bb430c7
scout <34626346+luceboxed@users.noreply.github.com>
2025-03-15 00:42:56 -0400

    silly typo

--- site/script.js
+++ site/script.js
@@ -1,4 +1,4 @@
-const products = {"temperature": "2m Temperature", "comp_reflectivity": "Composite Reflectivity", "wind": "10m Wind", "pressure": "MSLP", "cape": "CAPE", "cape_mu": "CAPE (Mixed Layer)", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
+const products = {"temperature": "2m Temperature", "comp_reflectivity": "Composite Reflectivity", "wind": "10m Wind", "pressure": "MSLP", "cape": "CAPE", "cape_mu": "CAPE (Most Unstable)", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
 const productSelector = document.getElementById('productSelector');
 for (key in products) {
     const option = document.createElement('option');

4910ce42c0e4dc10b9dfa48956b71b101bfed8fd
scout <34626346+luceboxed@users.noreply.github.com>
2025-03-15 00:04:14 -0400

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,4 +1,4 @@
-const products = {"temperature": "Temperature", "comp_reflectivity": "Composite Reflectivity", "wind": "Wind", "pressure": "Pressure", "cape": "CAPE", "cape_mu": "CAPE (Mixed Layer)", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
+const products = {"temperature": "2m Temperature", "comp_reflectivity": "Composite Reflectivity", "wind": "10m Wind", "pressure": "MSLP", "cape": "CAPE", "cape_mu": "CAPE (Mixed Layer)", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
 const productSelector = document.getElementById('productSelector');
 for (const key in products) {
     const option = document.createElement('option');

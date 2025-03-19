b1b29fed3f48b993ba35d101bfdd2587d0d14f26
scout <34626346+luceboxed@users.noreply.github.com>
2025-03-19 02:28:53 -0400

    add nearby NWS sites and remove CAPE vars
    we can compare soundings and forecasts
    
    also AFWA cape variables just don't work, so they outta here

--- script/meteogram.py
+++ script/meteogram.py
@@ -15,24 +15,20 @@ def plot_meteogram(wrf_file, airport, coords, output_path, forecast_times, wrfho
     dewpoints = []
     wind_speeds = []
     pressures = []
-    precipitations = []
     for t in hours:
         t_data = getvar(wrf_file, "T2", timeidx=t)[y, x].values
         q_data = getvar(wrf_file, "Q2", timeidx=t)[y, x].values
         wspd_data = getvar(wrf_file, "WSPD10MAX", timeidx=t)[y, x].values
         pressure_data = getvar(wrf_file, "AFWA_MSLP", timeidx=t)[y, x].values
-        precip_data = getvar(wrf_file, "RAINC", timeidx=t)[y, x].values
         t_f = (t_data - 273.15) * 9/5 + 32
         td = mpcalc.dewpoint(mpcalc.vapor_pressure(getvar(wrf_file, 'PSFC', timeidx=t) / 100 * units.mbar, q_data))
         td_f = to_np(td)[y, x].magnitude * 9/5 + 32
         wspd = to_np(wspd_data) * 2.23694
         pressure_mb = to_np(pressure_data) / 100
-        precip = to_np(precip_data) * 0.0393701 
         temperatures.append(t_f)
         dewpoints.append(td_f)
         wind_speeds.append(wspd)
         pressures.append(pressure_mb)
-        precipitations.append(precip)
     fig, ax1 = plt.subplots(figsize=(10, 6))
     ax1.plot(hours, temperatures, color='red', label='Temperature (°F)')
     ax1.plot(hours, dewpoints, color='green', label='Dewpoint (°F)')
@@ -44,10 +40,6 @@ def plot_meteogram(wrf_file, airport, coords, output_path, forecast_times, wrfho
     ax2 = ax1.twinx()
     ax2.plot(hours, pressures, color='blue', label='Pressure (mb)')
     ax2.set_ylabel('Pressure (mb)')
-    ax3 = ax1.twinx()
-    ax3.bar(hours, precipitations, color='dodgerblue', alpha=0.4, label='Precipitation (in)')
-    ax3.set_ylabel('Precipitation (in)')
-    ax3.spines['right'].set_position(('outward', 60))
     fig.legend(loc="lower right")
     plt.title(f"UGA-WRF Meteogram for {airport.upper()} starting at {forecast_times[1]} UTC")
     plt.grid(True)
--- site/script.js
+++ site/script.js
@@ -1,4 +1,4 @@
-const products = {"temperature": "2m Temperature", "dewp": "2m Dewpoint", "comp_reflectivity": "Composite Reflectivity", "wind": "10m Wind", "pressure": "MSLP", "cape": "CAPE", "cape_mu": "CAPE (Most Unstable)", "cin": "CIN", "cin_mu": "CIN (Most Unstable)", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
+const products = {"temperature": "2m Temperature", "dewp": "2m Dewpoint", "comp_reflectivity": "Composite Reflectivity", "wind": "10m Wind", "pressure": "MSLP", "helicity": "Helicity", "total_precip": "Total Precipitation", "snowfall": "Snowfall", "echo_tops": "Echo Tops"};
 const productSelector = document.getElementById('productSelector');
 for (key in products) {
     const option = document.createElement('option');
@@ -16,11 +16,14 @@ const meteogram = document.getElementById('meteogram')
 const textForecast = document.getElementById('textForecast');
 const sahn = document.getElementById('sahn');
 const scni = document.getElementById('scni');
-const satl = document.getElementById('satl');
-const sffc = document.getElementById('sffc');
+const satl = document.getElementById('satl')
 const smcn = document.getElementById('smcn');
 const srmg = document.getElementById('srmg');
 const scsg = document.getElementById('scsg');
+const sffc = document.getElementById('sffc');
+const seet = document.getElementById('seet');
+const sohx = document.getElementById('sohx');
+const sgsp = document.getElementById('sgsp');
 function updateImage() {
     const run = document.getElementById('runSelector').value;
     const product = productSelector.value;
@@ -30,10 +33,13 @@ function updateImage() {
     sahn.src = `runs/${run}/skewt/ahn/hour_${timestep}.png`;
     scni.src = `runs/${run}/skewt/cni/hour_${timestep}.png`;
     satl.src = `runs/${run}/skewt/atl/hour_${timestep}.png`;
-    sffc.src = `runs/${run}/skewt/ffc/hour_${timestep}.png`;
     smcn.src = `runs/${run}/skewt/mcn/hour_${timestep}.png`;
     srmg.src = `runs/${run}/skewt/rmg/hour_${timestep}.png`;
     scsg.src = `runs/${run}/skewt/csg/hour_${timestep}.png`;
+    sffc.src = `runs/${run}/skewt/ffc/hour_${timestep}.png`;
+    seet.src = `runs/${run}/skewt/bmx/hour_${timestep}.png`;
+    sohx.src = `runs/${run}/skewt/ohx/hour_${timestep}.png`;
+    sgsp.src = `runs/${run}/skewt/gsp/hour_${timestep}.png`;
 }
 function updateTextForecast() {
     const textSelector = document.getElementById('textSelector').value;

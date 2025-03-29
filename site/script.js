6dfa8894df4222726d1dc9184efb1bbb2561e663
scout <34626346+luceboxed@users.noreply.github.com>
2025-03-28 23:09:40 -0400

    TWO?!???!

--- site/script.js
+++ site/script.js
@@ -1,3 +1,32 @@
+// the dreaded products key value pair system returns. it is currently used for the secondary map system (which will be made better with time)
+const products = {
+    "temperature": "Temperature",
+    "dewp": "Dew Point",
+    "pressure": "Pressure",
+    "wind": "Wind",
+    "wind_gust": "Wind Gust",
+    "comp_reflectivity": "Composite Reflectivity",
+    "helicity": "Helicity",
+    "1hr_precip": "1-Hour Precipitation",
+    "total_precip": "Total Precipitation",
+    "1hr_snowfall": "1-Hour Snowfall",
+    "snowfall": "Snowfall",
+    "cloudcover": "Cloud Cover",
+    "echo_tops": "Echo Tops",
+    "temp_850mb": "Temperature (850mb)",
+    "temp_700mb": "Temperature (700mb)",
+    "temp_500mb": "Temperature (500mb)",
+    "temp_300mb": "Temperature (300mb)",
+    "td_850mb": "Dew Point (850mb)",
+    "td_700mb": "Dew Point (700mb)",
+    "td_500mb": "Dew Point (500mb)",
+    "td_300mb": "Dew Point (300mb)",
+    "wind_850mb": "Wind (850mb)",
+    "wind_700mb": "Wind (700mb)",
+    "wind_500mb": "Wind (500mb)",
+    "wind_300mb": "Wind (300mb)"
+};
+
 const outputs = "https://storage.googleapis.com/uga-wrf-website/outputs/";
 //const outputs = "runs/"
 const hours = 24;
@@ -14,6 +43,10 @@ const textForecast = document.getElementById('textForecast');
 const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
 const speedSelector = document.getElementById("speedSelector");
+const multiEnabler = document.getElementById("multiEnabler")
+const multiSelector = document.getElementById("multiSelector")
+const multiSubchooser = document.getElementById('multiSubchooser')
+const secondaryImage = document.getElementById('secondaryImage')
 const stationIds = ["sahn", "scni", "satl", "smcn", "srmg", "scsg", "sffc", "sbmx", "sohx", "sgsp"];
 const stationElements = Object.fromEntries(
     stationIds.map(id => [id, document.getElementById(id)])
@@ -44,6 +77,20 @@ function updateImage(selectedProduct = product) {
     stationIds.forEach(id => {
         stationElements[id].src = `${outputs}${run}/skewt/${id.replace('s', '')}/hour_${timestep}.png`;
     });
+    updateSecondaryDisplay()
+}
+function updateSecondaryDisplay() {
+    const run = runSelector.value;
+    const subchoosed = multiSubchooser.value
+    timestep = Number(slider.value);
+    if (multiSelector.value == 'map')
+        {
+            secondaryImage.src = `${outputs}${run}/${subchoosed}/hour_${timestep}.png`
+        }
+    else if (multiSelector.value == 'skewt')
+        {
+            secondaryImage.src = `${outputs}${run}/skewt/${subchoosed}/hour_${timestep}.png`
+        }
 }
 async function updateTextForecast() {
     const run = runSelector.value;
@@ -55,6 +102,38 @@ async function updateTextForecast() {
         });
     meteogram.src = `${outputs}${run}/meteogram/${textOption}/meteogram.png`;
 }
+function toggleSecondaryDisplay() {
+    if (multiEnabler.checked == true) {
+        secondaryImage.setAttribute('style', 'display:')
+        multiSelector.disabled = false
+        multiSubchooser.disabled = false
+        multiSubchooser.innerHTML = ''
+        if (multiSelector.value == 'map')
+            {
+                for (const key in products) {
+                    const option = document.createElement('option');
+                    option.value = key;
+                    option.textContent = products[key];
+                    multiSubchooser.appendChild(option);
+                }
+            }
+        else if (multiSelector.value == 'skewt') {
+            for (const airport in stationIds) {
+                const option = document.createElement('option');
+                option.value = stationIds[airport].replace('s', '')
+                option.textContent = stationIds[airport].replace('s', '')
+                multiSubchooser.appendChild(option)
+            }
+        }
+        updateSecondaryDisplay()
+    }
+    if (multiEnabler.checked == false) {
+        secondaryImage.setAttribute('style', 'display: none;')
+        multiSelector.disabled = true
+        multiSubchooser.disabled = true
+    }
+}
+
 document.querySelectorAll('.dropdown-content a').forEach(item => {
     item.addEventListener('click', event => {
         event.preventDefault();
@@ -70,6 +149,9 @@ textSelector.addEventListener('change', updateTextForecast);
 weatherImage.addEventListener('click', () => slider.focus());
 textForecast.addEventListener('click', () => textSelector.focus());
 meteogram.addEventListener('click', () => textSelector.focus());
+multiEnabler.addEventListener('click', toggleSecondaryDisplay)
+multiSelector.addEventListener('change', toggleSecondaryDisplay)
+multiSubchooser.addEventListener('change', updateSecondaryDisplay)
 let loopInterval;
 function startLoop() {
     if (timestep === hours) timestep = 0;
@@ -96,4 +178,7 @@ window.onload = function () {
     timestep = Number(slider.value);
     updateImage("temperature");
     updateTextForecast();
+    multiEnabler.checked = false
+    multiSelector.disabled = true
+    multiSubchooser.disabled = true
 };

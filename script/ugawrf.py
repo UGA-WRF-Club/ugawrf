57fdccff32809fc58c390c086619c539ceecd8d5
Scout C. <34626346+luceboxed@users.noreply.github.com>
2026-01-22 16:15:59 -0500

    Update ugawrf.py

--- script/ugawrf.py
+++ script/ugawrf.py
@@ -103,12 +103,6 @@ other_airports = {
 #^^^ temporarily disabling this due to our new small domain. will rewrite to handle empty extents later
 
 PRODUCTS = {
-    "afwasnow": "AFWA_SNOW",
-    "afwasnow_k": "AFWA_SNOW",
-    "afwarain": "AFWA_RAIN",
-    "afwafrz": "AFWA_FZRA",
-    "afwaslt": "AFWA_ICE",
-    "ptype": "AFWA_SNOW",
     "temperature": "T2",
     "1hr_temp_c": "T2",
     "dewp": "td2",

d84de10f6da7f386479cdaa16e0dc6abad841841
Scout C. <34626346+luceboxed@users.noreply.github.com>
2026-02-05 14:38:38 -0500

    wind barbs for apparent temp

--- script/weathermaps.py
+++ script/weathermaps.py
@@ -72,6 +72,7 @@ def plot_variable(product, variable, timestep, output_path, forecast_times, airp
         contour = ax.contourf(to_np(lons), to_np(lats), to_np(data_copy), cmap='nipy_spectral', levels=np.arange(-10, 110, 5), extend='both')
         plot_title = f'2m Apparent Temperature (°F) - Hour {f_hour}\nValid: {valid_time_str}\nInit: {init_str}'
         label = f'Temperature (°F)'
+        plot_wind_barbs(ax, wrf_file, timestep, lons, lats)
     elif product == 'dewp':
         if not partial_bool and not process_all:
             print(f'-> skipping {product} {timestep} due to partial flag being disabled')

2d1583d0a2a007c0a12576ffc88ba601afe89f3c
scout teehee <34626346+luceboxed@users.noreply.github.com>
2026-03-05 23:06:22 -0500

    really poor first pass of meteogram plotting per day

--- script/meteogram.py
+++ script/meteogram.py
@@ -6,6 +6,7 @@ import matplotlib.patheffects as path_effects
 from wrf import getvar, ll_to_xy, to_np
 import numpy as np
 import os
+from adjustText import adjust_text
 
 def plot_meteogram(wrf_file, airport, coords, output_path, forecast_times, wrfhours, run_time):
     x, y = ll_to_xy(wrf_file, coords[0], coords[1])
@@ -27,14 +28,6 @@ def plot_meteogram(wrf_file, airport, coords, output_path, forecast_times, wrfho
         dewpoints.append(td_f)
         pressures.append(pressure_mb)
     fig, ax1 = plt.subplots(figsize=(10, 6))
-    maxtemp_x = np.argmax(temperatures)
-    mintemp_x = np.argmin(temperatures)
-    maxdew_x = np.argmax(dewpoints)
-    mindew_x = np.argmin(dewpoints)
-    max_temp = temperatures[maxtemp_x]
-    min_temp = temperatures[mintemp_x]
-    max_dew = dewpoints[maxdew_x]
-    min_dew = dewpoints[mindew_x] 
     ax1.plot(hours, temperatures, color='red', label='Temp (°F)')
     ax1.plot(hours, dewpoints, color='green', label='Dewp (°F)')
     if any(t <= 32 for t in temperatures):
@@ -50,19 +43,48 @@ def plot_meteogram(wrf_file, airport, coords, output_path, forecast_times, wrfho
     ax1.set_xlabel('Hour (UTC)') 
     ax1.set_xticks(hours)
     ax1.set_xticklabels(times, rotation=45)
-    ax1.annotate(f"{max_temp:.1f} F", xy=(maxtemp_x, max_temp), xytext=(maxtemp_x + 1, max_temp), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
-    ax1.annotate(f"{min_temp:.1f} F", xy=(mintemp_x, min_temp), xytext=(mintemp_x + 1, min_temp), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
-    ax1.annotate(f"{max_dew:.1f} F", xy=(maxdew_x, max_dew), xytext=(maxdew_x + 1, max_dew), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
-    ax1.annotate(f"{min_dew:.1f} F", xy=(mindew_x, min_dew), xytext=(mindew_x + 1, min_dew), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    # will turn this into an actual function later, but just implementing first pass
+    start1, end1 = 1, 24
+    maxtemp_x_24 = np.argmax(temperatures[start1:end1]) + start1
+    mintemp_x_24 = np.argmin(temperatures[start1:end1]) + start1
+    maxdew_x_24 = np.argmax(dewpoints[start1:end1]) + start1
+    mindew_x_24 = np.argmin(dewpoints[start1:end1]) + start1
+    max_temp_24 = temperatures[maxtemp_x_24]
+    min_temp_24 = temperatures[mintemp_x_24]
+    max_dew_24 = dewpoints[maxdew_x_24]
+    min_dew_24 = dewpoints[mindew_x_24] 
+    ax1.annotate(f"{max_temp_24:.1f} F", xy=(maxtemp_x_24, max_temp_24), xytext=(maxtemp_x_24 + 1, max_temp_24), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{min_temp_24:.1f} F", xy=(mintemp_x_24, min_temp_24), xytext=(mintemp_x_24 + 1, min_temp_24), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{max_dew_24:.1f} F", xy=(maxdew_x_24, max_dew_24), xytext=(maxdew_x_24 + 1, max_dew_24), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{min_dew_24:.1f} F", xy=(mindew_x_24, min_dew_24), xytext=(mindew_x_24 + 1, min_dew_24), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    start2, end2 = 24, 48
+    maxtemp_x_48 = np.argmax(temperatures[start2:end2]) + start2
+    mintemp_x_48 = np.argmin(temperatures[start2:end2]) + start2 
+    maxdew_x_48 = np.argmax(dewpoints[start2:end2]) + start2 
+    mindew_x_48 = np.argmin(dewpoints[start2:end2]) + start2
+    max_temp_48 = temperatures[maxtemp_x_48]
+    min_temp_48 = temperatures[mintemp_x_48]
+    max_dew_48 = dewpoints[maxdew_x_48]
+    min_dew_48 = dewpoints[mindew_x_48]
+    ax1.annotate(f"{max_temp_48:.1f} F", xy=(maxtemp_x_48, max_temp_48), xytext=(maxtemp_x_48 + 1, max_temp_48), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{min_temp_48:.1f} F", xy=(mintemp_x_48, min_temp_48), xytext=(mintemp_x_48 + 1, min_temp_48), color='red', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{max_dew_48:.1f} F", xy=(maxdew_x_48, max_dew_48), xytext=(maxdew_x_48 + 1, max_dew_48), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax1.annotate(f"{min_dew_48:.1f} F", xy=(mindew_x_48, min_dew_48), xytext=(mindew_x_48 + 1, min_dew_48), color='green', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
     ax2 = ax1.twinx()
-    maxpressure_x = np.argmax(pressures)
-    minpressure_x = np.argmin(pressures)
-    max_pressure = pressures[maxpressure_x]
-    min_pressure = pressures[minpressure_x]
     ax2.plot(hours, pressures, color='blue', label='Pressure (mb)')
     ax2.set_ylabel('MSLP (mb)')
-    ax2.annotate(f"{max_pressure:.1f} mb", xy=(maxpressure_x, max_pressure), xytext=(maxpressure_x + 1, max_pressure), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
-    ax2.annotate(f"{min_pressure:.1f} mb", xy=(minpressure_x, min_pressure), xytext=(minpressure_x + 1, min_pressure), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    maxpressure_x_24 = np.argmax(pressures[start1:end1]) + start1
+    minpressure_x_24 = np.argmin(pressures[start1:end1]) + start1
+    max_pressure_24 = pressures[maxpressure_x_24]
+    min_pressure_24 = pressures[minpressure_x_24]
+    ax2.annotate(f"{max_pressure_24:.1f} mb", xy=(maxpressure_x_24, max_pressure_24), xytext=(maxpressure_x_24 + 1, max_pressure_24), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax2.annotate(f"{min_pressure_24:.1f} mb", xy=(minpressure_x_24, min_pressure_24), xytext=(minpressure_x_24 + 1, min_pressure_24), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    maxpressure_x_48 = np.argmax(pressures[start2:end2]) + start2
+    minpressure_x_48 = np.argmin(pressures[start2:end2]) + start2
+    max_pressure_48 = pressures[maxpressure_x_48]
+    min_pressure_48 = pressures[minpressure_x_48]
+    ax2.annotate(f"{max_pressure_48:.1f} mb", xy=(maxpressure_x_48, max_pressure_48), xytext=(maxpressure_x_48 + 1, max_pressure_48), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
+    ax2.annotate(f"{min_pressure_48:.1f} mb", xy=(minpressure_x_48, min_pressure_48), xytext=(minpressure_x_48 + 1, min_pressure_48), color='blue', fontsize=14, ha='center', path_effects=[path_effects.withStroke(linewidth=1, foreground="black")],)
     ax1.axvline(x=24, color='black', linestyle='--', linewidth=0.5, label='FHR24')
     lines_ax1, labels_ax1 = ax1.get_legend_handles_labels()
     lines_ax2, labels_ax2 = ax2.get_legend_handles_labels()
